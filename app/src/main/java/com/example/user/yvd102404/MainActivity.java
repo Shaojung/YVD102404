f8550e052ee3d764322764ad5a8014130dd1cc96
Shaojung <shaojung@gmail.com>
2016-10-24 15:04:17 +0800

    Self layout simple adapter

--- app/src/main/java/com/example/user/yvd102404/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102404/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     Spinner spinner2;
     String fruit[] = {"蘋果","葡萄","芭樂","芒果","甘蔗","鳳梨"};
+    String cities[] = {"台北", "台中","高雄"};
+    String codes[] = {"02", "04", "07"};
     ArrayAdapter<String> adapter;
     ArrayList<Map<String, String>> mylist;
     SimpleAdapter adapter2;
@@ -30,18 +32,14 @@ public class MainActivity extends AppCompatActivity {
         // ======================
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         mylist = new ArrayList<>();
-        HashMap<String,String> m1 = new HashMap<>();
-        m1.put("city", "台北");
-        m1.put("code", "02");
-        mylist.add(m1);
-        HashMap<String,String> m2 = new HashMap<>();
-        m2.put("city", "台中");
-        m2.put("code", "04");
-        mylist.add(m2);
-        HashMap<String,String> m3 = new HashMap<>();
-        m3.put("city", "高雄");
-        m3.put("code", "07");
-        mylist.add(m3);
+        for (int i=0;i<cities.length;i++)
+        {
+            HashMap<String,String> m1 = new HashMap<>();
+            m1.put("city", cities[i]);
+            m1.put("code", codes[i]);
+            mylist.add(m1);
+        }
+
         adapter2 = new SimpleAdapter(MainActivity.this,
                     mylist,
                     R.layout.myitem,
